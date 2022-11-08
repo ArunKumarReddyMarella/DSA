1ae4024a5be0d186f866a7609f54395e41972e42
ArunKumarReddyMarella <arunkumarreddy159@gmail.com>
2022-11-08 20:49:27 +0530

    kth set bit

---
+++ bitManipulation/check kth bit set/java/CheckKthbitSet.java
@@ -0,0 +1,47 @@
+import java.util.*;
+import java.io.*;
+public class CheckKthbitSet {
+    private static boolean isKthbitSet(int number,int k){
+        if(k < 0)
+            return false;
+        //left shift
+        // if ((number & (1 << (k - 1))) != 0)
+		// 	return true;
+		// return false;
+
+        // right shift
+        if (((number >> (k - 1)) & 1) == 1) 
+		    return true; 
+		return false;
+        
+    }
+    public static void main(String[] args) {
+        FileInputStream fis;
+		FileOutputStream fos;
+		BufferedReader br;
+		PrintWriter pw;
+		StringTokenizer st;
+
+
+		try{
+		fis = new FileInputStream("./../input.txt");
+		fos = new FileOutputStream("./../output.txt");
+
+		br = new BufferedReader(new InputStreamReader(fis));
+		
+		pw = new PrintWriter(fos);
+		String line;
+
+		while((line=br.readLine())!=null){
+			st = new StringTokenizer(line);
+			int number = Integer.parseInt(st.nextToken());
+            int k = Integer.parseInt(st.nextToken());
+            pw.println("bit in position " + k + " of number " + number+" is "+isKthbitSet(number, k));
+		}
+		pw.close();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+    }
+}
